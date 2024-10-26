38cd6d85e1d8195e6a77919307cbf1b2821df2b6
SofiaRumeo <sofiarumeo3@gmail.com>
2024-10-26 15:59:21 -0300

    Fundamentos de java

---
+++ src/repasofundamentos/java/RepasoFundamentosJava.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasofundamentos.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sofia
+ */
+public class RepasoFundamentosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        // OPERACIONES ARITMETICOS +, -, *, /, %(resto
+        //int numa = 17;
+        //int numb  = 3;
+        //int nume = 14;
+    
+        //int suma = numa + numb;
+    
+        //sout(numa - nume);
+        //sout(numa + " + " + numb + " = " + suma);
+    
+        //OPERADORES LOGICOS
+        // AND - OR - NOT
+    
+        //OPERADORES RELACIONALES
+        // < > == != ===
+    
+        //sout("RELACIONALES")
+        // int numUnoRel = 2;
+        // int numDos Rel = 2;
+        // int numTresRel = 8;
+        // int numCuatroRel = 15;
+    
+    
+        // sout(numUnoRel == numTresRel && numCuatroRel==numTresRel);
+        
+        
+        //OPERADORES DE INCREMENTO Y DECREMENTO
+        // VAR++ VAR--
+        System.out.println("OPERADORES DE INCREMENTO Y DECREMENTO");
+        int numIncDec = 0;
+        numIncDec++;
+        numIncDec++;
+        numIncDec++;
+        numIncDec--;
+        System.out.println(numIncDec);
+        
+        // ENTRADA DE DATOS
+        // clasescanner - ingresa datos
+        
+        Scanner leer = new Scanner(System.in);
+        //Nombre
+        System.out.println("Ingrese su nombre: ");
+        String nombre = leer.nextLine();
+        
+        
+        //Edad
+        
+        System.out.println("Ingrese su edad ");
+        int edadScanner = leer.nextInt();
+        
+        
+        //Email
+        System.out.println("Ingrese su email: ");
+        String email = leer.nextLine();
+        
+        //System.out.printf("Mi edad es %d, y me llamo %s ", y mi correo es %s ", edadScanner, nombre, emailito);
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    }   
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
+    
+} 
